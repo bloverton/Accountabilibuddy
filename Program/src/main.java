450ebd5ed48e60162b362e1b2478d5450d85e51e
hrag <hrag.ayvazian.793@my.csun.edu>
2017-02-14 10:32:42 -0800

    first commit

--- Program/src/main.java
+++ Program/src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Test123);
+        System.out.println("Test123");
     }
 }
