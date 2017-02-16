219b19e419faa1bdf81d541e873c575342542b14
Benjamin Overton <overtonbenjamin@gmail.com>
2017-02-15 23:10:18 -0800

    Added FXML files for login screen

---
+++ Program/src/Controllers/LoginScreenController.java
@@ -0,0 +1,7 @@
+package Controllers;
+
+/**
+ * Created by overt on 2/15/2017.
+ */
+public class LoginScreenController {
+}
---
+++ Program/src/LoginScreen.java
@@ -0,0 +1,24 @@
+/**
+ * Created by overt on 2/15/2017.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginScreen extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScreenView.fxml"));
+        primaryStage.setTitle("Accountabilibuddy");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
