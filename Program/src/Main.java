c1775d541ddbf644fc4937c37a3759674a5db9e3
Benjamin Overton <overtonbenjamin@gmail.com>
2017-02-16 08:08:44 -0800

    Delete Main.java

--- Program/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Test123");
-        for(int i=0; i<=10; i++) {
-            System.out.println("Hello, World!");
-        }
-    }
-}
