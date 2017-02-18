14b9ee70b03a5c5b6a32e7fd6c434d9b7abf6bb9
Benjamin Overton <overtonbenjamin@gmail.com>
2017-02-18 13:58:58 -0800

    Post Login Screen Files
    Added controllers and FXML files for post login

---
+++ Program/src/accountabilibuddy/Main.java
@@ -0,0 +1,51 @@
+package accountabilibuddy;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application{
+
+    private Scene currentScene;
+    private Stage currentStage;
+
+    private Parent root;
+
+    public static void main(String[] args) throws Exception{
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        this.currentStage = primaryStage;
+        this.currentStage.setTitle("Accountabilibuddy");
+        initLoginScreen();
+    }
+
+    /**
+     * Initializes login scene when program first runs
+     */
+    private void initLoginScreen(){
+        swapRoot("view/LoginScreenView.fxml");
+    }
+
+    /**
+     * Swaps root to another fxml file and swaps primary scene
+     * @param fxmlFilePath - Location and name of FXML file
+     */
+    public void swapRoot(String fxmlFilePath) {
+        try {
+            root = FXMLLoader.load(getClass().getResource(fxmlFilePath));
+            currentScene = new Scene(root);
+            currentStage.setScene(currentScene);
+            currentStage.setResizable(false);
+            currentStage.show();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+}
